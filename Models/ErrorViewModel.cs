569dc452eee480266a4bb94b6292d0454d552b91
Tadeo Gavensky <tadeogavensky@gmail.com>
2025-06-11 19:51:24 -0300

    primer commit del tp

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PNT1_TP_Cine.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
