76c3ebe5324b4517bc0f07eeb97ad3afc4700e62
Tadeo Gavensky <tadeogavensky@gmail.com>
2025-06-12 20:05:00 -0300

    home empezada y controllers

---
+++ Controllers/PeliculasController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PNT1_TP_Cine.Models;
+
+namespace PNT1_TP_Cine.Controllers
+{
+    public class PeliculasController : Controller
+    {
+        private readonly Context _context;
+
+        public PeliculasController(Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var peliculas = _context.Peliculas.ToList(); 
+            ViewBag.Peliculas = peliculas;
+            return View();
+        }
+
+
+    }
+
+}
