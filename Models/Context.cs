c6651b3f85f8b847ed6e83385ab80a67bf04686c
adrian.addesso <adrian.addesso@gmail.com>
2025-06-20 16:43:38 -0300

    0 changes

--- Models/Context.cs
+++ Models/Context.cs
@@ -14,7 +14,7 @@ namespace PNT1_TP_Cine.Models
         public DbSet<Sala> Salas { get; set; } = null!;
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=DESKTOP-PKM95E0\\MSSQLSERVER01;" +
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-PKM95E0\\MSSQLSERVER01;Initial Catalog=PNT1_TP1_Cine;" +
                            " Integrated Security= true; TrustServerCertificate= true; Encrypt= true");
             base.OnConfiguring(optionsBuilder);
 
