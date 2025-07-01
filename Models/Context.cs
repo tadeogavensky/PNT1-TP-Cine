d5b39ff610e29e00f2e5666e9e920c4f0eb0308e
piloxciv <robertolevyadtr@gmail.com>
2025-07-01 18:26:32 -0300

    Ultimos ajustes diseño, agregado favicon y resolución de conflicon rama roberto-dev

---
+++ Models/Context.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace PNT1_TP_Cine.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Funcion> Funciones { get; set; } = null!;
+        public DbSet<Pelicula> Peliculas { get; set; } = null!;
+        public DbSet<Ticket> Tickets { get; set; } = null!;
+        public DbSet<Usuario> Usuarios { get; set; } = null!;
+        public DbSet<Rol> Roles { get; set; } = null!;
+        public DbSet<Genero> Generos { get; set; } = null!;
+        public DbSet<Sala> Salas { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=PNT1_TP1_Cine;" +
+               " Integrated Security= true; TrustServerCertificate= true; Encrypt= true");
+
+        }
+    }
+}
